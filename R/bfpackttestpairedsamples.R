10382402fb1257ec7046c96ed699894cba5c8e80
Julius Pfadt <julius.pfadt@gmail.com>
2025-06-23 09:44:11 +0200

    fix empty variables error

--- R/bfpackttestpairedsamples.R
+++ R/bfpackttestpairedsamples.R
@@ -24,7 +24,7 @@ bfpackTTestPairedSamples <- function(jaspResults, dataset, options, ...) {
   ready <- .bfpackOptionsReady(options, type)
 
   # handle the data set
-  dataset <- .bfpackHandleData(dataset, options)
+  dataset <- dataset <- .bfpackHandleData(dataset, options, ready = ready)
 
   # Check if current data allow for analysis
   .bfpackDataReady(dataset, options, type, ready)
